58c1810c6c86f9cf86c0b85da02b36b49f964c55
User0k <glucus2@yandex.ru>
2023-04-16 15:36:41 +0300

    fix: add conditional url wrapper to imgMinifyer

--- client/src/utils/imgMinifyer.ts
+++ client/src/utils/imgMinifyer.ts
@@ -1,8 +1,10 @@
 import { IMAGE_MIN_CROP } from '../constants';
 
-export default function imgMinifyer(images: string[]) {
+export default function imgMinifyer(images: string[], urlInclude = true) {
   return images.map((image) => {
     const params = image.split('?');
-    return params[0] + IMAGE_MIN_CROP;
+    return urlInclude
+      ? `url(${params[0]}${IMAGE_MIN_CROP})`
+      : params[0] + IMAGE_MIN_CROP;
   });
 }
