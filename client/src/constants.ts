caafe70d869a5622603a6866ca45ecfeeb05b3a5
User0k <glucus2@yandex.ru>
2023-04-16 15:41:57 +0300

    feat: add layout modal for board creation

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -6,3 +6,16 @@ export const BACKGROUND_IMG_SOURCE =
 export const IMAGES_LOAD_AMOUNT = 6;
 export const IMAGE_MIN_CROP =
   '?crop=entropy&cs=tinysrgb&fit=crop&h=200&q=80&utm_campaign=api-credit&w=400';
+
+export const minColorHue = 100;
+
+export const PRELOADED_IMAGES = [
+  'https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?crop=entropy&w=1920',
+  'https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?crop=entropy&w=1920',
+  'https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?crop=entropy&cs=tinysrgb&fit=crop&h=1080&q=80&utm_campaign=api-credit&w=1920',
+  'https://images.unsplash.com/photo-1504198266287-1659872e6590?crop=entropy&w=1920',
+  'https://images.unsplash.com/photo-1490750967868-88aa4486c946?crop=entropy&w=1920',
+  'https://images.unsplash.com/photo-1431794062232-2a99a5431c6c?crop=entropy&w=1920',
+  'https://images.unsplash.com/photo-1458929526027-052f5d6a3c5e?crop=entropy&w=1920',
+  'https://images.unsplash.com/photo-1448518340475-e3c680e9b4be?crop=entropy&w=1920',
+];
--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,4 @@
+import { IMAGES_LOAD_AMOUNT, PRELOADED_IMAGES } from '../../constants';
 import {
   useGetAllBoardsQuery,
   useGetBoardByIdQuery,
@@ -5,6 +6,9 @@ import {
   useUpdateBoardMutation,
   useDeleteBoardMutation,
 } from '../../services/boardService';
+import { useState } from 'react';
+import imgMinifyer from '../../utils/imgMinifyer';
+import { getAllSources } from '../../utils/backgroundRandomizer';
 import Board from '../../components/Board';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -13,11 +17,27 @@ import Button from '@mui/material/Button';
 import AppsIcon from '@mui/icons-material/Apps';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import './MainPage.scss';
 import Spinner from '../../components/Spinner';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import TextField from '@mui/material/TextField';
+import DialogActions from '@mui/material/DialogActions';
+import './MainPage.scss';
 
 function MainPage() {
   const { data: boards, isLoading: isBoardsLoading } = useGetAllBoardsQuery();
+  const preloadMinifiedImgs = imgMinifyer(PRELOADED_IMAGES);
+  const [images, setImages] = useState(preloadMinifiedImgs);
+  const [createBoard] = useCreateBoardMutation();
+  const [open, setOpen] = useState(true);
+  const handleClickOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const loadMoreImages = async () => {
+    const imgArr: string[] = await getAllSources(IMAGES_LOAD_AMOUNT);
+    setImages(imgArr);
+  };
+
   return (
     <Box id="main">
       <Box sx={{ pt: 3 }}>
@@ -50,9 +70,60 @@ function MainPage() {
             {boards?.map((board) => (
               <Board {...board} key={board.id} />
             ))}
-            <Button sx={{mb: 2}} className="btn-create-board">create a board</Button>
+            <Button
+              sx={{ mb: 2 }}
+              className="btn-create-board"
+              onClick={handleClickOpen}>
+              create a board
+            </Button>
           </Stack>
         )}
+        <Dialog open={open} onClose={handleClose} className="create-board">
+          <DialogTitle className="create-board__title">
+            Create a board
+          </DialogTitle>
+          <DialogContent sx={{ p: 2 }}>
+            <Box
+              sx={{ backgroundImage: `${images[0]}` }}
+              className="create-board__preview"
+            />
+            <Stack
+              direction={'row'}
+              justifyContent={'space-between'}
+              className="background-change__wrapper">
+              <Button variant="contained" size="small">
+                Choose image
+              </Button>
+              <Button variant="contained" size="small">
+                Choose gradient
+              </Button>
+            </Stack>
+            <TextField
+              autoFocus
+              required
+              id="name"
+              label="Board name"
+              fullWidth
+              size="small"
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              required
+              id="description"
+              label="Board description"
+              fullWidth
+              size="small"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button type="submit" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="contained" onClick={handleClose}>
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Container>
     </Box>
   );
