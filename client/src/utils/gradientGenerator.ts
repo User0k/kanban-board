f453739bf396522e6c25849bcf3c3c64acbb1d94
User0k <glucus2@yandex.ru>
2023-04-18 23:03:39 +0300

    feat: implement gradient generator functions

---
+++ client/src/utils/gradientGenerator.ts
@@ -0,0 +1,31 @@
+import { minColorHue } from '../constants';
+
+const generateChannel = () =>
+  minColorHue + Math.floor(Math.random() * (256 - minColorHue));
+const chooseEmptyChannel = () => Math.floor(Math.random() * 3);
+
+function createColor() {
+  const emptyChannel = chooseEmptyChannel();
+  const firstChannel = generateChannel();
+  const secondChannel = generateChannel();
+  const color = [firstChannel, secondChannel];
+  color.splice(emptyChannel, 0, 0);
+  return color;
+}
+
+function generateGradient() {
+  const firstColor = createColor();
+  const secondColor = createColor();
+  return `linear-gradient(0deg, rgba(${firstColor + ''}, 1) 0%, rgba(${
+    secondColor + ''
+  }, 1) 100%)`;
+}
+
+export function getAllGradients(n: number) {
+  const gradients: string[] = [];
+  for (let i = 0; i < n; i++) {
+    gradients.push(generateGradient());
+  }
+
+  return gradients;
+}
