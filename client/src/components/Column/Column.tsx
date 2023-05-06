8a896e0f4468a323bef525e790b7d8cf0538af13
User0k <glucus2@yandex.ru>
2023-05-06 23:48:37 +0300

    feat: get tasks in column, implement task creation

--- client/src/components/Column/Column.tsx
+++ client/src/components/Column/Column.tsx
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 import { IColumn } from '../../models';
+import { useGetTasksInColumnQuery } from '../../services/taskService';
 import ColumnTitleInput from './ColumnTitleInput';
 import ColumnTitleText from './ColumnTitleText';
+import NewTaskModal from '../modals/NewTaskModal';
+import Task from '../Task';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import AddIcon from '@mui/icons-material/Add';
 import './Column.scss';
 
 function Column({ boardId, id, title }: IColumn) {
   const [columnName, setColumnName] = useState(title);
   const [isEditing, setIsEditing] = useState(false);
+  const [isTaskCreating, setIsTaskCreating] = useState(false);
   const onCloseInput = () => setIsEditing(false);
   const onOpenInput = () => setIsEditing(true);
+  const { data: tasks } = useGetTasksInColumnQuery({ boardId, columnId: id });
 
   return (
     <Box className="column">
@@ -35,12 +41,23 @@ function Column({ boardId, id, title }: IColumn) {
           />
         )}
       </Stack>
-      <Button
-        className="btn-create_task"
-        variant="contained"
-        startIcon={<AddIcon />}>
-        Add task
-      </Button>
+      <Box className="task-list">
+        {(tasks || []).map((task) => (
+          <Task key={task.id} />
+        ))}
+      </Box>
+      {isTaskCreating && <CircularProgress color="success" />}
+      <NewTaskModal
+        boardId={boardId}
+        columnId={id}
+        setIsTaskCreating={setIsTaskCreating}>
+        <Button
+          className="btn-create_task"
+          variant="contained"
+          startIcon={<AddIcon />}>
+          Add task
+        </Button>
+      </NewTaskModal>
     </Box>
   );
 }
