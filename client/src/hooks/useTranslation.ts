129735499c8df00c7c73ccd896522c0564106386
User0k <glucus2@yandex.ru>
2023-12-22 23:39:17 +0300

    feat: i18n basic implementation

---
+++ client/src/hooks/useTranslation.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { Translation } from '../locales/types';
+
+export const useTranslation = (lang: string) => {
+  const [translation, setTranslation] = useState<Translation | null>(null);
+
+  useEffect(() => {
+    const fetchTranslation = async () => {
+      try {
+        const module = await import(`../locales/${lang}`);
+        setTranslation(module[lang]());
+      } catch (error) {
+        console.error('This language is unsupportable');
+        setTranslation(null);
+      }
+    };
+
+    fetchTranslation();
+  }, [lang]);
+
+  return translation;
+};
---
+++ client/src/locales/en/index.ts
@@ -0,0 +1,11 @@
+
+export const en = () => ({
+  header: {
+    home: 'Home',
+    main: 'Boards',
+    profile: 'Profile',
+    registration: 'Sign up',
+    login: 'Sign in',
+    logout: 'Log out',
+  },
+});
